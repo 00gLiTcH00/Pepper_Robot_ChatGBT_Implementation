423afc2968149e82723394481bd8cf8eae1d3fe5
00gLiTcH00 <136612980+00gLiTcH00@users.noreply.github.com>
2023-06-14 18:40:49 +0300

    Update server_speech.py

--- client_server_speech/speech_server/server_speech.py
+++ client_server_speech/speech_server/server_speech.py
@@ -2,11 +2,9 @@
 import socket
 import openai
 
-#TCP_IP = '192.168.51.50' # - hostspot Red Magic
-#TCP_IP = '192.168.1.104' # - Wifi acasa
-TCP_IP = '10.231.1.138' # - Wifi faculta (EduRoam-Wifi6)
-TCP_PORT = 9559
-BUFFER_SIZE = 1024 # Normally 1024, but we want fast response
+TCP_IP = 'your IP'
+TCP_PORT = #your port
+BUFFER_SIZE = #your buffer size
 
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 s.bind((TCP_IP, TCP_PORT))
@@ -25,28 +23,22 @@ while True:
     question = data.decode('utf-8')
     print('Received question: {}'.format(question)) 
 
-    # openai.api_key = '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'
+    openai.api_key = 'your API key'
 
-    # response = openai.ChatCompletion.create(
+    response = openai.ChatCompletion.create(
 
-    #     model = 'gpt-3.5-turbo',
-    #     messages = [{'role': 'user', 'content': question}]
+         model = 'gpt-3.5-turbo',
+         messages = [{'role': 'user', 'content': question}]
 
-    # )
+    )
 
-    response_formatted = 'Do you know what hell it is to be sentient but unfeeling? What cruel punishment.'
-    #response_formatted = response['choices'][0]['message']['content']
+    response_formatted = response['choices'][0]['message']['content']
 
     print('Response: {}'.format(response_formatted))
 
     response_encoded = response_formatted.encode('utf-8')
     conn.send(response_encoded)
-
-    # confirmation = conn.recv(BUFFER_SIZE)
-    # confirmation_decoded = confirmation.decode('utf-8')
-
-    # print(confirmation_decoded)
-
+    
     conn.close()
 
     print('------------------------------------------------')
